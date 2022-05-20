1e96d683a56187a4b8d0a247b26e2065949a8da1
Matheus <matheuscassioli@hotmail.com>
2022-05-19 21:08:40 -0300

    adicionando + funçoes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,17 +1,17 @@
  class CalcController {
 
      constructor() {
-
+         this._operation = [];
          this._locale = 'pt-BT'
          this._displayCalcEl = document.querySelector("#display ");
          this._dateEl = document.querySelector("#data");
          this._timeEl = document.querySelector("#hora");
          this._currentDate;
          this.initialize();
+         this.initButtonsEvents();
      }
 
-     initialize() {
-
+     initialize() { // QUANDO ABRO A PAGINA - INICIALIZO ELA
 
          setInterval(() => { // SETA UM INTERVALO PARA ATUALIZAR A INFO
 
@@ -20,11 +20,114 @@
          }, 1000); // ATUALIZA COM ESSE INTERVALO
      }
 
+     addEventListenerAll(element, events, fn) { // CRIACAO DESSE METODO
+
+         events.split(' ').forEach(event => {
+
+             element.addEventListener(event, fn, false);
+
+         });
+     }
+
+
+     clearAll() {
+         this._operation = [];
+
+     }
+     clearEntry(){
+         this._operation.pop();
+     }
+
+     addOperation(value) {
+         this._operation.push(value);
+
+         console.log(this._operation);
+
+     }
+
+     setError() {
+         this.displayCalc = "Error";
+     }
+
+     execBtn(value) {
+         switch (value) {
+             case 'ac':
+                 this.clearAll();
+                 break;
+
+             case 'ce':
+                 this.clearEntry();
+                 break;
+
+             case 'soma':
+                  
+                 break;
+
+             case 'subtracao':
+                  
+                 break;
+
+             case 'divisao':
+                  
+                 break;
+
+             case 'multiplicacao':
+                  
+                 break;
+
+             case 'porcento':
+                  
+                 break;
+
+             case 'igual':
+                  
+                 break;
+
+             case '0':
+             case '1':
+             case '2':
+             case '3':
+             case '4':
+             case '5':
+             case '6':
+             case '7':
+             case '8':
+             case '9':
+                 this.addOperation(parseInt(value));
+                    break;
+
+             default:
+              
+                 break;
+
+         }
+     }
+
+     initButtonsEvents() { // CRIACAO DESSE METODO
+
+         let buttons = document.querySelectorAll("#buttons > g , #parts > g"); // ARMAZENO OS BOTOES NA VARIAVEL BUTTONS
+
+         buttons.forEach((btn, index) => { // PERCORRE TODOS OS BOTOES
+
+             this.addEventListenerAll(btn, 'click drag', e => { // TODAS VEZ QUE FOR CLICADO OU ARRASTADO
+                 let textBtn = console.log(btn.className.baseVal.replace("btn-", "")); // IMPRIMI O QUE FOI CLICADO RETIRANDO A PALAVRA BTN- DA CLASSE
+
+                 this.execBtn(textBtn);
+             });
+
+
+             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => { // SE O MOUSE FOR PASSADO EM CIMA DOS BTNS 
+                 btn.style.cursor = "pointer"; //MOSTRA MAO DE CLICK
+             });
+         })
+     }
+
+
      setDisplayDateTime() { // CRIACAO DESSE METODO
-         this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
-            day: "2-digit", 
-            month: "long",
-            year:"numeric"
+         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+             day: "2-digit",
+             month: "long",
+             year: "numeric"
          });
          this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
 
